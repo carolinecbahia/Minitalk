6e296f27e6b2b2cf01521014f397aae0f091d264
Caroline Bahia <carolinecbahia@gmail.com>
2025-10-29 18:39:29 -0300

    10-29 starter at 42SP

--- client.c
+++ client.c
@@ -6,13 +6,20 @@
 /*   By: ccavalca <ccavalca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 15:06:41 by ccavalca          #+#    #+#             */
-/*   Updated: 2025/10/29 17:13:51 by ccavalca         ###   ########.fr       */
+/*   Updated: 2025/10/29 18:19:29 by ccavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
-
+	if (argc != 2)
+	{
+		printf("Error.\n The string must be bt \" \" ");
+	}
+	else
+	printf("Teste de validação: %s", argv[1]);
+	
 }
